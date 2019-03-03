c6b1df5f5326dda52c4f11de99caa2921a3318d5
jkjok <normalno1234@gmail.com>
2019-03-03 20:22:58 +0300

    yes

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: '755809310:AAEJ4deRc2tBLSh24a9IpaVjiXkDBO-NX3A',
+    admin: '549073144',
+    yandex: 'https://money.yandex.ru/to/999999999999999/',
+    qiwi: 'https://qiwi.com/payment/form/99?extra%5B%27account%27%5D=79999999999&amountInteger=',
+};
